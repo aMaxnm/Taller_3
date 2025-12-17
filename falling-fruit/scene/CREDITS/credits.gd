35769d29cc9f5a4c0e2173a99272b64ec7e94382
Maximiliano Núñez <antonio.nunez25@uabc.edu.mx>
2025-12-17 15:51:10 -0800

    credits

---
+++ falling-fruit/scene/CREDITS/credits.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+var menu_scene = load("res://scene/menu/menu.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_packed(menu_scene)
--- falling-fruit/scene/menu/menu.gd
+++ falling-fruit/scene/menu/menu.gd
@@ -1,6 +1,7 @@
 extends MarginContainer
 
 var main_level_scene = preload("res://scene/main_level/main_level.tscn")
+var credit_scene = preload("res://scene/CREDITS/credits.tscn")
 var bg_music = load("res://assets/music/Pianta Village - Super Mario Sunshine Music Extended.mp3")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,3 +19,7 @@ func _on_start_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_creditos_pressed() -> void:
+	get_tree().change_scene_to_packed(credit_scene)
